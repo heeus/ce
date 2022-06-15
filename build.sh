c9aa59265fc79d61fafcdc246bbe077b9c69bddd
maxim-ge <maxim.ge@gmail.com>
2022-06-15 15:45:49 +0300

    build works

--- build.sh
+++ build.sh
@@ -2,27 +2,48 @@
 
 set -Eeuo pipefail
 
-function cleanup {      
-  echo bye
-}
-trap cleanup EXIT
-
 [ -z ${1-} ] && { echo "use build.sh VersionCore [PreRelease]"; exit 1; }
+
+BuildFolder=".build"
+
+# Process arguments
 
 VersionCore=$1
 PreRelease=${2-}
 [ -z ${PreRelease-} ] && SemVer=$VersionCore || SemVer=$VersionCore-$PreRelease
 
-[ -d "./.build" ] || mkdir ./.build
+# Functions
 
-pushd . > /dev/null
-cd cli
-env GOOS=linux GOARCH=amd64 go build -o ../.build/ce
-popd > /dev/null
+function cleanup {      
+  echo bye
+}
 
-pushd .build > /dev/null
-tar -czvf ce_linux_amd64.tar.gz ce
-popd > /dev/null
+function build_os_arch {
+  pushd . > /dev/null
+  cd cli
+  env GOOS=$1 GOARCH=$2 go build -o ../$BuildFolder/ce
+  popd > /dev/null
 
-cp install/install.sh .build
+  pushd $BuildFolder > /dev/null
+  tar -czvf ce_v${SemVer}_$1_$2.tar.gz ce > /dev/null
+  popd > /dev/null
+  rm $BuildFolder/ce
+}
 
+# End of functions
+
+trap cleanup EXIT
+
+# Cleanup
+
+rm -rf $BuildFolder
+[ -d "$BuildFolder" ] || mkdir $BuildFolder
+
+# Build os+arch
+
+build_os_arch linux amd64
+build_os_arch linux 386
+build_os_arch freebsd amd64
+build_os_arch freebsd 386
+
+cp install/install.sh $BuildFolder
